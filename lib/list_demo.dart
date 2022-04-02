baa9a74a347a1207780aa92aa07c0a4bca1ce47d
橋本和磨 <kazumahashimoto0421@gmail.com>
2022-04-02 20:07:37 +0900

    3ページ目に企画のカードを作ってみた

--- lib/list_demo.dart
+++ lib/list_demo.dart
@@ -6,12 +6,56 @@ class ListDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('3ページ目'),
-      ),
-      body: Container(
-        color: Colors.lime,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('3ページ目'),
+        ),
+        body: Center(
+            child: Container(
+          // padding: EdgeInsets.symmetric(vertical: 20, horizontal: 13),
+          height: 100,
+          child: Card(
+            elevation: 3,
+            color: Colors.cyan,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 13),
+              child: Row(
+                children: [
+                  Container(
+                    color: Colors.black38,
+                    height: 60,
+                    width: 60,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        '井上研究室',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      Text(
+                        'プログラミング',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'オンライン',
+                            style: TextStyle(fontSize: 9),
+                          ),
+                          Text(
+                            '場所',
+                            style: TextStyle(fontSize: 9),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )));
   }
 }
