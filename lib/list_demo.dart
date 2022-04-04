e81bff3eff1bb2944027e3fb6a1cd96936a4d0ff
橋本和磨 <kazumahashimoto0421@gmail.com>
2022-04-05 00:43:24 +0900

    背景つけたりアイコンつけたり

--- lib/list_demo.dart
+++ lib/list_demo.dart
@@ -9,69 +9,96 @@ class ListDemo extends StatelessWidget {
         appBar: AppBar(
           title: const Text('3ページ目'),
         ),
-        body: Center(
-            child: Container(
-          height: 100,
-          child: Card(
-            elevation: 3,
-            color: Colors.cyan,
-            child: Container(
-              padding: EdgeInsets.only(left: 13, top: 15, right: 13),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 15),
-                    height: 60,
-                    width: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.black38,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        '井上研究室',
-                        style: TextStyle(fontSize: 11),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(bottom: 5),
-                        height: 45,
-                        child: const FittedBox(
-                          fit: BoxFit.scaleDown,
-                          child: Text(
-                            'プログラミング',
-                            style: TextStyle(
-                              fontSize: 17,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'オンライン',
-                            style: TextStyle(fontSize: 9),
-                          ),
-                          Text(
-                            '場所',
-                            style: TextStyle(fontSize: 9),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 40,
-                  ),
+        body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+                colors: [
+                  const Color(0xffe4a972).withOpacity(0.6),
+                  const Color(0xff9941d8).withOpacity(0.6),
+                ],
+                stops: const [
+                  0.0,
+                  1.0,
                 ],
               ),
             ),
-          ),
-        )));
+            child: Center(
+                child: SizedBox(
+              height: 100,
+              child: Card(
+                elevation: 3,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 13, top: 15, right: 13),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 15),
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.black38,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '井上研究室',
+                            style: TextStyle(fontSize: 11),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(bottom: 5),
+                            height: 45,
+                            child: const FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                'プログラミング',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.place,
+                                color: Colors.blueAccent,
+                                size: 9,
+                              ),
+                              Text(
+                                'オンライン',
+                                style: TextStyle(fontSize: 9),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Icon(
+                                Icons.place,
+                                color: Colors.blueAccent,
+                                size: 9,
+                              ),
+                              Text(
+                                '場所',
+                                style: TextStyle(fontSize: 9),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 40,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ))));
   }
 }
